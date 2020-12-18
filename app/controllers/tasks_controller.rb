11c79a0b62ae79ee92d6ffbb6903ec2daea5a585
kohei.shiga <koheiandres381022@gmail.com>
2020-12-18 09:58:29 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
     
     def index
-        @tasks = Task.all
+        @tasks = current_user.tasks.order(id: :desc)
     end
     
     def show
@@ -13,7 +14,7 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = "新規タスクを作成しました。"
@@ -47,7 +48,10 @@ class TasksController < ApplicationController
     
     private
     def set_task
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
     end
     
     def task_params
