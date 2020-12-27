b0a6d1d9853d035f49407335d7bf2ba0a05779f1
kohei.shiga <koheiandres381022@gmail.com>
2020-12-27 07:53:57 +0000

    modify users_controller.rb create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      session[:user_id] = @user.id
       flash[:success] = 'ユーザーを登録しました。'
-      redirect_to tasks_path
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザー登録に失敗しました。'
       render :new
